9c812310f9ce414fcf3bc297303d49115409b0f6
Jeremy <jeremy-wong1998@hotmail.com>
2023-08-29 03:28:47 +0800

    Home Page Base

--- src/app/(nav)/home/page.tsx
+++ src/app/(nav)/home/page.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
 
-const handleButtonClick = (buttonName: string) => {
-  console.log(`Button ${buttonName} clicked`);
-  // You can add your desired logic here for each button
-};
-
-const MyPage: React.FC = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">Button Page</h1>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
-        onClick={() => handleButtonClick('Button 1')}
-      >
-        Button 1
+const YourComponent: React.FC = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+    <button className="bg-blue-500 text-white text-xl font-semibold py-48 px-16 rounded-lg mb-10 hover:bg-blue-600" style={{ width: '100%' }}>
+      Scan to Earn Points
+    </button>
+    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+      <button className="bg-blue-500 text-white text-lg font-semibold py-24 px-20 rounded-lg hover:bg-blue-600" style={{ width: '49%' }}>
+        Rewards
       </button>
-      <button
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-2"
-        onClick={() => handleButtonClick('Button 2')}
-      >
-        Button 2
-      </button>
-      <button
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-2"
-        onClick={() => handleButtonClick('Button 3')}
-      >
-        Button 3
+      <button className="bg-blue-500 text-white text-lg font-semibold py-24 px-20 rounded-lg hover:bg-blue-600" style={{ width: '49%' }}>
+        Recycling Locations
       </button>
     </div>
-  );
-};
+  </div>
+);
 
-export default MyPage;
+export default YourComponent;
