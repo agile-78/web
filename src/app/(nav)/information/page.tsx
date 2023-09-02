a01f78881a5c8c8a224d837d10784929a14f6162
WhiteCl0ud01 <136679162+WhiteCl0ud01@users.noreply.github.com>
2023-09-02 19:55:16 +0800

    main branch edit

---
+++ src/app/(nav)/information/page.tsx
@@ -0,0 +1,205 @@
+import React, { useState } from 'react';
+
+interface RecyclingInfo {
+  imageUrl: string;
+  title: string;
+}
+
+const PlasticInfos: RecyclingInfo[] = [
+  {
+    imageUrl: '/info-images/beverage.png',
+    title: 'Beverage bottles'
+  },
+  {
+    imageUrl: '/info-images/detergent.png',
+    title: 'Detergent bottles',
+  },
+  {
+    imageUrl: '/info-images/bag.png',
+    title: 'Plastic bags',
+  },
+  {
+    imageUrl: '/info-images/shampoo.png',
+    title: 'Shampoo bottles',
+  },
+  {
+    imageUrl: '/info-images/packWfoil.png',
+    title: 'Packaging with foil',
+  },
+  {
+    imageUrl: '/info-images/mel.jpeg',
+    title: 'Melamine products',
+  },
+  {
+    imageUrl: '/info-images/card.png',
+    title: 'Expired credit card',
+  },
+  {
+    imageUrl: '/info-images/toys.png',
+    title: 'Toys',
+  }
+];
+
+const AluminiumInfos: RecyclingInfo[] = [
+    {
+      imageUrl: '/info-images/cans.png',
+      title: 'Aluminum cans',
+    },
+    {
+      imageUrl: '/info-images/foil.png',
+      title: 'Aluminum foil',
+    },
+    {
+      imageUrl: '/info-images/tray.png',
+      title: 'Aluminum trays',
+    },
+    {
+      imageUrl: '/info-images/aluBot.jpeg',
+      title: 'Aluminum bottles',
+    },
+    {
+      imageUrl: '/info-images/aluCon.png',
+      title: 'Aluminum containers',
+    },
+    {
+      imageUrl: '/info-images/lids.png',
+      title: 'Aluminum lids',
+    },
+    {
+      imageUrl: '/info-images/tubes.png',
+      title: 'Aluminum tubes',
+    },
+    {
+      imageUrl: '/info-images/package.png',
+      title: 'Aluminum packaging',
+    },
+  ];
+  
+  const PaperInfos: RecyclingInfo[] = [
+    {
+      imageUrl: '/info-images/news.png',
+      title: 'Newspapers',
+    },
+    {
+      imageUrl: '/info-images/box.png',
+      title: 'Cardboard boxes',
+    },
+    {
+      imageUrl: '/info-images/maga.png',
+      title: 'Magazines',
+    },
+    {
+      imageUrl: '/info-images/office.png',
+      title: 'Office paper',
+    },
+    {
+      imageUrl: '/info-images/junk.png',
+      title: 'Junk mail',
+    },
+    {
+      imageUrl: '/info-images/Pbag.png',
+      title: 'Paper bags',
+    },
+    {
+      imageUrl: '/info-images/cereal.png',
+      title: 'Cereal boxes',
+    },
+    {
+      imageUrl: '/info-images/phone.png',
+      title: 'Phone books',
+    },
+  ];
+
+  const RecyclingSection: React.FC<{
+    title: string;
+    recyclableItems: RecyclingInfo[];
+    nonRecyclableItems: RecyclingInfo[];
+    extraCan: string;
+    extraNot: string;
+  }> = ({ title, recyclableItems, nonRecyclableItems, extraCan, extraNot }) => (
+<div className="mt-8 bg-gray-200 p-4 rounded-lg shadow-md">
+  <div className="text-4xl sm:text-6xl font-semibold mb-2 text-center text-blue-500">{title}</div>
+  <h2 className="text-3xl sm:text-5xl font-semibold mb-2 text-green-600">OK to recycle</h2>
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
+    {recyclableItems.map((recyclingInfo, index) => (
+      <div key={index} className="bg-white p-4 sm:p-8 rounded-lg shadow-md">
+        <div className="mb-2 sm:mb-4 flex items-center justify-center">
+          <img
+            className="w-12 h-12 sm:w-16 sm:h-16 mr-2 sm:mr-4"
+            src={recyclingInfo.imageUrl}
+            alt={recyclingInfo.title}
+          />
+        </div>
+        <div className="text-center">
+          <h1 className="text-lg sm:text-xl font-semibold">{recyclingInfo.title}</h1>
+        </div>
+      </div>
+    ))}
+  </div>
+  <p className="text-xl sm:text-2xl text-center mt-6 sm:mt-8">
+    <b>
+      <u>Other items that can be recycled:</u><br />{extraCan}
+    </b>
+  </p>
+
+  <br />
+
+  <h2 className="text-3xl sm:text-5xl font-semibold mb-2 text-red-600">NOT OK to recycle</h2>
+  <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
+    {nonRecyclableItems.map((recyclingInfo, index) => (
+      <div key={index} className="bg-white p-4 sm:p-8 rounded-lg shadow-md">
+        <div className="mb-2 sm:mb-4 flex items-center justify-center">
+          <img
+            className="w-12 h-12 sm:w-16 sm:h-16 mr-2 sm:mr-4"
+            src={recyclingInfo.imageUrl}
+            alt={recyclingInfo.title}
+          />
+        </div>
+        <div className="text-center">
+          <h1 className="text-lg sm:text-xl font-semibold">{recyclingInfo.title}</h1>
+        </div>
+      </div>
+    ))}
+  </div>
+  <p className="text-xl sm:text-2xl text-center mt-6 sm:mt-8">
+    <b>
+      <u>Other items that cannot be recycled:</u><br />{extraNot}
+    </b>
+  </p>
+</div>
+
+  );
+  
+  const YourComponent: React.FC = () => (
+    <div>
+        <RecyclingSection
+        title="PLASTIC"
+        recyclableItems={PlasticInfos.slice(0, 4)}
+        nonRecyclableItems={PlasticInfos.slice(4, 8)}
+        extraCan={'Ziplock bags, bubble wraps, egg trays, fruit box and film packagings'}
+        extraNot={'Plastic packaging contaminated with food'}
+        />
+  
+      <RecyclingSection
+        title="ALUMINIUM"
+        recyclableItems={AluminiumInfos.slice(0, 4)}
+        nonRecyclableItems={AluminiumInfos.slice(4, 8)}
+        extraCan={'Aluminum cans, foil, pie tins'}
+        extraNot={'Foil with food residue, aluminum cans with hazardous waste'}
+      />
+  
+      <RecyclingSection
+        title="PAPER"
+        recyclableItems={PaperInfos.slice(0, 4)}
+        nonRecyclableItems={PaperInfos.slice(4, 8)}
+        extraCan={'Clean paper, cardboard, newspapers'}
+        extraNot={'Greasy or food-soiled paper, laminated paper, tissues'}
+      />
+  
+      <br /><br /><br /><br /><br />
+    </div>
+  );
+  
+  export default YourComponent;
+  
+  
--- src/app/(nav)/profile/page.tsx
+++ src/app/(nav)/profile/page.tsx
@@ -3,17 +3,22 @@ import Link from "next/link";
 
 import profilepic from "../../../../public/profile.png";
 
-export default function profile() { 
-    return ( 
+export default function profile() {
+  return (
     <main className="bg-white">
-
       <div className="flex flex-col justify-center items-center w-screen h-screen">
-          <Image src={ profilepic } alt="Profile picture" className="px-4 mx-auto w-auto h-[20%]" />
+        <Image
+          src={profilepic}
+          alt="Profile picture"
+          className="px-4 mx-auto w-auto h-[20%]"
+        />
         <p className="text-2xl">User</p>
         <div className="bg-[#FBFDB9] w-[90%] p-4 mb-3 text-left rounded-lg h-[20%]">
-            <p className="text-2xl font-bold">Your points </p>
-            <p className="text-2xl pl-1 font-bold">50 points collected </p>
-            <p className="text-l pt-3">You have a 10 days recycle streak, keep it up!</p>
+          <p className="text-2xl font-bold">Your points </p>
+          <p className="text-2xl pl-1 font-bold">50 points collected </p>
+          <p className="text-l pt-3">
+            You have a 10 days recycle streak, keep it up!
+          </p>
         </div>
         <div className="w-[90%] py-2 pl-1 text-left  h-[10%]">
           <p className="text-2xl">Your statistics</p>
@@ -22,17 +27,20 @@ export default function profile() {
           <p className="text-xl py-1 font-bold">Recycled 20 times</p>
         </div>
         <div className="w-[90%] py-2 pl-1 text-left h-[10%]">
-          <Link href="/profile/edit"><p className="text-xl py-2">Edit Profile &gt;</p></Link>
+          <Link href="/profile/edit">
+            <p className="text-xl py-2">Edit Profile &gt;</p>
+          </Link>
         </div>
         <div className="w-[90%] py-2 pl-1 text-left h-[10%]">
           <p className="text-xl py-2">Check Rewards &gt;</p>
         </div>
         <div className="w-[90%] py-2 pl-1 text-left h-[10%]">
-            <Link href="/referral"><p className="text-xl py-2">My referral code &gt;</p></Link>
+          <Link href="/referral">
+            <p className="text-xl py-2">My referral code &gt;</p>
+          </Link>
         </div>
-        <div className="h-[15%]">
-        </div>
+        <div className="h-[15%]"></div>
       </div>
-    </main> 
-    ) 
+    </main>
+  );
 }
