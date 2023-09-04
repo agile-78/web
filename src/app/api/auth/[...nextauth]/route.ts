1895ed6c71de8be9c88357563c365df9310c2027
Thar Lin Shwe <albent2017@gmail.com>
2023-09-04 16:54:45 +0800

    configured next-auth to maintain session

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-// import { authenticate } from "@/services/authService"
 import { authenticate } from "@/services/authService";
 import NextAuth from "next-auth";
 import type { AuthOptions } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
@@ -23,13 +23,13 @@ export const authOptions: AuthOptions = {
             return null;
           }
 
-          const data = res.data;
+          const { token, ...data } = res.data;
 
           if (typeof res !== "undefined") {
             if (res.status === 200 && res.data)
               return {
                 ...data,
-                apiToken: data.token,
+                apiToken: token,
               };
           } else {
             return null;
@@ -45,6 +45,31 @@ export const authOptions: AuthOptions = {
     signIn: "/auth/login",
     newUser: "/home",
     error: "/auth",
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      const sanitizedToken = Object.keys(token).reduce((p, c) => {
+        // strip unnecessary properties
+        if (c !== "iat" && c !== "exp" && c !== "jti" && c !== "apiToken") {
+          return { ...p, [c]: token[c] };
+        } else {
+          return p;
+        }
+      }, {});
+
+      return {
+        ...session,
+        user: sanitizedToken,
+        apiToken: token.apiToken,
+      };
+    },
+    async jwt({ token, user }) {
+      if (typeof user !== "undefined") {
+        return user as unknown as JWT;
+      }
+
+      return token;
+    },
   },
 };
 
