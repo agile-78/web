b7ddaf5b6e3d3fa988c0b8e7e0433b8d7e726487
Thar Lin Shwe <albent2017@gmail.com>
2023-09-04 15:55:41 +0800

    Implemented login form

---
+++ src/components/auth/form/login.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { Button } from "@/components/button";
+import { signIn } from "next-auth/react";
+import { FormEvent } from "react";
+
+export function LoginForm() {
+  interface data {
+    email: string;
+    password: string;
+  }
+  const submitHandler = async (val: data) => {
+    await signIn("credentials", { ...val });
+  };
+
+  return (
+    <form
+      onSubmit={async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const formProps = Object.fromEntries(formData);
+        await submitHandler(formProps as unknown as data);
+      }}
+    >
+      <input
+        type="text"
+        name="email"
+        className="text-gray-500 px-3 py-2 border border-gray-400 rounded-lg my-5"
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        name="password"
+        className="text-gray-500 px-3 py-2 border border-gray-400 rounded-lg mb-5"
+        placeholder="Password"
+      />
+      <Button>Sign In</Button>
+    </form>
+  );
+}
