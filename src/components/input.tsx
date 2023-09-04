02e493d99fea86be463e9316f43fabfdd45015ad
Thar Lin Shwe <albent2017@gmail.com>
2023-09-04 15:56:42 +0800

    Updated input component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -4,11 +4,13 @@ type Props = {
   type?: HTMLInputTypeAttribute;
   placeholder?: string;
   className?: string;
+  name?: string;
 };
 
-export function Input({ type = "text", placeholder, className }: Props) {
+export function Input({ name, type = "text", placeholder, className }: Props) {
   return (
     <input
+      name={name}
       type={type}
       className={`w-full text-gray-500 px-3 py-2 border border-gray-400 rounded-lg ${className}`}
       placeholder={placeholder}
