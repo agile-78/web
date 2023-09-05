6d59eb8b058de9d80ca1d919438e9f4c9de50037
Thar Lin Shwe <albent2017@gmail.com>
2023-09-05 17:06:18 +0800

    Implmented axios helper methods

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,3 +4,35 @@ import axios from "axios";
 export const axiosClient = axios.create({
   baseURL: API_URI,
 });
+
+export const post = async (url: string, body: any, token?: string) => {
+  try {
+    if (token) {
+      return await axiosClient.post(url, body, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+
+    return await axiosClient.post(url, body);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const get = async (url: string, token?: string) => {
+  try {
+    if (token) {
+      return await axiosClient.get(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
+
+    return await axiosClient.get(url);
+  } catch (e) {
+    console.log(e);
+  }
+};
