5aff47cec69f769b04c4dcc41ee029c09e83bc5f
Thar Lin Shwe <albent2017@gmail.com>
2023-09-05 17:07:26 +0800

    Implemented Reward component

---
+++ src/components/reward.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { ClaimBtn } from "./claimBtn";
+import { API_URI, BASE_API_URI } from "@/constants";
+
+interface IProp {
+  _id: string;
+  src: string;
+  alt: string;
+  title: string;
+  points: number;
+}
+
+export function Reward({ src, alt, title, points, _id }: IProp) {
+  return (
+    <div className="bg-[#FFB6B6] bg-opacity-50 w-[90%] rounded-lg p-4 text-black flex items-center justify-between py-4">
+      <Image
+        src={`${BASE_API_URI}/${src}`}
+        alt={alt}
+        width={0}
+        height={0}
+        className="w-1/4 h-1/3 p1-2 pt-2"
+      />
+      <div>
+        <p className="font-bold">{title}</p>
+        <p className="text-lg">{points} points</p>
+      </div>
+      <ClaimBtn _id={_id} />
+    </div>
+  );
+}
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
 export const API_URI = process.env.NEXT_PUBLIC_API_URI || "";
-
+export const JWT_SECRET = process.env.JWT_SECRET || "test";
+export const BASE_API_URI = process.env.NEXT_PUBLIC_BASE_API_URI || "";
 if (!API_URI) {
   throw new Error("env variable NEXT_PUBLIC_API_URI is not given");
 }
