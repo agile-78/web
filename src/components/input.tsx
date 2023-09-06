1aff53b9ce8a5b42dfd9c3483f281a7a297e2b60
Thar Lin Shwe <albent2017@gmail.com>
2023-09-06 17:41:55 +0800

    Updated Input Component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,23 @@
 import { HTMLInputTypeAttribute } from "react";
 
-type Props = {
+export interface IInputProps {
   type?: HTMLInputTypeAttribute;
   placeholder?: string;
   className?: string;
   name?: string;
-};
+  defaultValue?: string;
+}
 
-export function Input({ name, type = "text", placeholder, className }: Props) {
+export function Input({
+  name,
+  type = "text",
+  placeholder,
+  className,
+  defaultValue,
+}: IInputProps) {
   return (
     <input
+      defaultValue={defaultValue}
       name={name}
       type={type}
       className={`w-full text-gray-500 px-3 py-2 border border-gray-400 rounded-lg ${className}`}
