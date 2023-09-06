4f1cdf88adab663180baa4b044da096a2be87e9a
Thar Lin Shwe <albent2017@gmail.com>
2023-09-06 17:44:30 +0800

    Updated edit profile page to send api request

--- src/components/form/edit.tsx
+++ src/components/form/edit.tsx
@@ -3,20 +3,55 @@
 import { FormEvent } from "react";
 import { Button } from "../button";
 import { Input } from "../input";
+import { PasswordInput } from "./passwordInput";
+import { useSession } from "next-auth/react";
+import { Session } from "next-auth";
+import { editProfile } from "@/services/userService";
 
 export function EditForm() {
+  const { data: session } = useSession();
+
+  const { user } = session as Session;
+
   const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+    const body: {
+      [key: string]: any;
+    } = {};
+
+    for (let key in data) {
+      if (
+        // @ts-ignore
+
+        data[key] == user[key] ||
+        (key === "password" && data[key] === "******")
+      ) {
+        continue;
+      } else {
+        body[key] = data[key];
+      }
+    }
+
+    await editProfile(user._id, body, session?.apiToken);
   };
 
   return (
-    <form className="mx-2">
-      <div className="flex">
-        <Input placeholder="Name" className="w-1/2 mr-2" />
-        <Input placeholder="Surname" className="w-1/2 ml-2" />
-      </div>
-      <Input placeholder="Username" className="mt-4" />
-      <Input placeholder="Email address" className="mt-4 mb-4" />
+    <form className="mx-2" onSubmit={submitHandler}>
+      <Input
+        defaultValue={user.name}
+        name="name"
+        placeholder="Name"
+        className="w-1/2 mr-2"
+      />
+      <Input
+        defaultValue={user.email}
+        name="email"
+        placeholder="example@gmail.com"
+        className="mt-4"
+      />
+      <PasswordInput name="password" className="mt-4" />
       <Button type="submit" className="mt-4">
         Done
       </Button>
