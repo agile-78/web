d60ac99a7607f7d446c64ecd71db53c477af157c
Thar Lin Shwe <albent2017@gmail.com>
2023-09-06 17:41:09 +0800

    typing for session

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,3 +1,4 @@
+import { IUser } from "@/interfaces/user";
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
@@ -5,11 +6,7 @@ declare module "next-auth" {
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
-      _id: string;
-      /** The user's postal address. */
-      address: string;
-    };
+    user: IUser;
     apiToken: string;
   }
 }
