0558147c16362d74203a122cd09fb7e1743853bd
WhiteCl0ud01 <136679162+WhiteCl0ud01@users.noreply.github.com>
2023-09-09 03:25:01 +0800

    Added the information button in homepage

--- src/app/(nav)/home/page.tsx
+++ src/app/(nav)/home/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
       <Head>
         <title>Home</title>
       </Head>
-      <div className="w-[90%] h-[50%]">
+      <div className="w-[90%] h-[45%]">
         <Link href="/camera">
-          <button className="bg-gradient-to-b from-[#34BBFB] to-[#E8F6FD] w-[100%] h-[85%] rounded-xl">
+          <button className="bg-gradient-to-b from-[#34BBFB] to-[#E8F6FD] w-[100%] h-[90%] rounded-xl">
             <div className="flex flex-col items-center justify-center w-[100%] h-[100%]">
               <p className="text-3xl font-bold h-[40%] flex items-center justify-center">
                 Scan to earn points
@@ -28,8 +28,8 @@ export default function Home() {
         </Link>
       </div>
 
-      <div className="flex w-[90%] h-[40%]">
-        <div className="w-[45%] h-[55%] mr-[5%]">
+      <div className="flex w-[90%] h-[20%]">
+        <div className="w-[45%] h-[90%] mr-[5%]">
           <Link href="/rewards">
             <button className="bg-[#E498FF] w-[100%] h-[85%] rounded-xl">
               <div className="flex flex-col items-center justify-center w-[100%] h-[100%]">
@@ -48,7 +48,7 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="w-[45%] h-[55%] ml-[5%]">
+        <div className="w-[45%] h-[90%] ml-[5%]">
           <Link href="/map">
             <button className="bg-[#E2FFB2] w-[100%] h-[85%] rounded-xl">
               <div className="flex flex-col items-center justify-center w-[100%] h-[100%]">
@@ -63,13 +63,28 @@ export default function Home() {
             </button>
           </Link>
           <p className="text-lg font-bold mt-1 flex items-center justify-center text-center">
-            Recycling Locations
+            Bins location
           </p>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
+      </div>
+
+      <div className="w-[90%] h-[30%]">
+        <Link href="/camera">
+          <button className="mt-4 bg-gradient-to-b from-[#F7FA43] to-[#FFFFE9] w-[100%] h-[60%] rounded-xl">
+            <div className="flex items-center justify-center w-[100%] h-[100%]">
+              <Image
+                src="/recycle-bin.png"
+                alt="Recycle Bin"
+                width={100}
+                height={100}
+                className="h-[60%] w-auto"
+              />
+              <p className="text-xl font-bold h-[40%] flex items-center justify-center">
+                Recycling Information
+              </p>
+            </div>
+          </button>
+        </Link>
       </div>
     </div>
   );
