e099c5c4505a0b3d81979faf6c0e160b96d0a817
WhiteCl0ud01 <136679162+WhiteCl0ud01@users.noreply.github.com>
2023-09-17 01:19:19 +0800

    done with image scanning

--- src/app/(nav)/camera/page.tsx
+++ src/app/(nav)/camera/page.tsx
@@ -4,17 +4,11 @@ import { NextPage } from 'next';
 
 const EjsTemplate: NextPage = () => {
   // Read the EJS template file and compile it
-  const ejsTemplate = readFileSync('./views/testing.ejs', 'utf-8');
+  const ejsTemplate = readFileSync('./views/camera.ejs', 'utf-8');
   const compiledTemplate = compile(ejsTemplate);
 
-  // Data to pass to the template
-  const data = {
-    title: 'My EJS Template',
-    message: 'Hello from EJS!',
-  };
-
   // Render the compiled EJS template
-  const renderedHtml = compiledTemplate(data);
+  const renderedHtml = compiledTemplate();
 
   return (
     <div dangerouslySetInnerHTML={{ __html: renderedHtml }} />
